916e00025b45d40dc47c55c947874e68c8a677bf
cdAkash <akashyadav15032002@gmail.com>
2025-03-10 10:33:56 +0530

    Project Setup completed

---
+++ TimeCapsuleFrontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { React } from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <Button>Click me</Button>
+      <h1>hello</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
